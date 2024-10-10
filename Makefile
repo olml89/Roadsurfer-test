367de0811951b8949ab207238266bb4689f982e9
olml89 <oleguer.ml89@gmail.com>
2024-10-10 16:05:36 +0200

    Configure docker-compose, upgrade PHP version to 8.3 and Symfony dependencies to 7.1

---
+++ Makefile
@@ -0,0 +1,15 @@
+.PHONY: build
+build:
+	docker-compose -f docker/docker-compose.yml build
+
+.PHONY: upd
+upd:
+	docker-compose -f docker/docker-compose.yml up -d --remove-orphans
+
+.PHONY: down
+down:
+	docker-compose -f docker/docker-compose.yml down
+
+.PHONY: ssh
+ssh:
+	docker-compose -f docker/docker-compose.yml exec php /bin/sh
--- config/bundles.php
+++ config/bundles.php
@@ -1,7 +1,12 @@
 <?php
 
+/**
+ * SensioFrameworkExtraBundle it's no longer recommended as all the annotations provided byh that bundle are now
+ * built-in in Symfony as PHP attributes:
+ * https://symfony.com/bundles/SensioFrameworkExtraBundle/current/index.html
+ * https://stackoverflow.com/questions/78617019/update-6-4-to-6-5-class-sensio-bundle-frameworkextrabundle-sensioframeworkextr
+ */
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
